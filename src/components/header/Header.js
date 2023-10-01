5120745677976d39c5f98ca0b8c1051e229fdd58
kfiddle <kenjfiddle@gmail.com>
2023-09-30 22:03:26 -0400

    first commit

---
+++ src/components/header/Header.js
@@ -0,0 +1,13 @@
+import styles from './Header.module.css';
+
+const Header = () => {
+  return (
+    <div>
+      <h1 style={{ color: 'white', fontSize: '5rem' }}>
+        <span style={{ fontSize: '6rem' }}>K</span>en <span style={{ fontSize: '6rem' }}>J</span>ohnston
+      </h1>
+    </div>
+  );
+};
+
+export default Header;
