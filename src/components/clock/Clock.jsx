033ec5f154cae487e832ba1ceee33c22d9d6e85a
kfiddle <kenjfiddle@gmail.com>
2024-02-18 18:44:51 -0500

    small adjustments

--- src/components/clock/Clock.jsx
+++ src/components/clock/Clock.jsx
@@ -4,24 +4,21 @@ import styles from "./Clock.module.css";
 const Clock = () => {
     const [positionX, setPositionX] = useState(0);
     const [positionY, setPositionY] = useState(-44);
-    const duration = 60000; 
+    const duration = 60000;
 
     useEffect(() => {
         const moveClockwise = (startTime) => {
             const currentAngle = ((Date.now() - startTime) / duration) * 360; // Angle in degrees
 
-            // Calculate new position based on angle and radius
             const newX = Math.cos((currentAngle * Math.PI) / 180) * 30;
             const newY = Math.sin((currentAngle * Math.PI) / 180) * 30;
 
-            // setAngle(currentAngle);
             setPositionX(newX);
             setPositionY(newY);
 
             if (Date.now() - startTime < duration) {
                 requestAnimationFrame(() => moveClockwise(startTime)); // Pass startTime parameter
             } else {
-                // setAngle(0);
                 setPositionX(0);
                 setPositionY(-44);
                 moveClockwise(Date.now());
@@ -29,7 +26,14 @@ const Clock = () => {
         };
 
         moveClockwise(Date.now());
+
     }, [duration]);
+
+    const currentDate = new Date();
+    let hour = currentDate.getHours() % 12 || 12; // Convert to 12-hour format
+    let minute = currentDate.getMinutes();
+    let hourRotation = ((hour * 30) + (minute / 2) - 90); // Hour hand rotation in degrees
+    let minuteRotation = (minute * 6) - 90; // Minute hand rotation in degrees
 
     return (
         <div className={styles.outerContainer}>
@@ -38,8 +42,8 @@ const Clock = () => {
             </div>
 
             <div className={styles.blackCenter}>
-                <div className={styles.hourHand} style={{ transform: `translate(45%, 0%) rotate(${320}deg)` }}></div>
-                <div className={styles.minuteHand} style={{ transform: `translate(45%, 0%) rotate(${100}deg)` }}></div>
+                <div className={styles.hourHand} style={{ transform: `translate(45%, 0%) rotate(${hourRotation}deg)` }}></div>
+                <div className={styles.minuteHand} style={{ transform: `translate(45%, 0%) rotate(${minuteRotation}deg)` }}></div>
             </div>
         </div>
     );
