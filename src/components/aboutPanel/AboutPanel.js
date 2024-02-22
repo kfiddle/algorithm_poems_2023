f243d59a2394c5e97760e0cf2882a973dea7d093
kfiddle <kenjfiddle@gmail.com>
2024-02-22 14:18:42 -0500

    aboutMe responsive

--- src/components/aboutPanel/AboutPanel.js
+++ src/components/aboutPanel/AboutPanel.js
@@ -2,47 +2,68 @@ import { useState, useEffect, Fragment } from 'react';
 
 import { useMediaQuery } from 'react-responsive';
 
-import deskStyles from './AboutPanel.module.css';
+import styles from './AboutPanel.module.css';
 import phoneStyles from './PhoneAboutPanel.module.css';
 
-const AboutPanel = (props) => {
-  const [leftCurtainPlace, setLeftCurtainPlace] = useState(-52);
-  const [rightCurtainPlace, setRightCurtainPlace] = useState(70);
+// extra small is < 576
+// small is 576 to 768
+// medium is 768 to 992
+// large is 992 to 1200
+// extra large is 1200+
 
-  const [imageFilter, setImageFilter] = useState(300);
+const AboutPanel = (props) => {
+
   const [leftCurtainOpacity, setLeftCurtainOpacity] = useState(0.2);
 
-  const isSmallish = useMediaQuery({ maxWidth: 1240 });
-  const isMobile = useMediaQuery({ maxWidth: 767 });
-  const small = useMediaQuery({ minWidth: 768, maxWidth: 992 });
-
-
-  const styles = !isSmallish ? deskStyles : phoneStyles;
+  const extraSmall = useMediaQuery({ maxWidth: 576 });
+  const small = useMediaQuery({ minWidth: 576, maxWidth: 767 });
+  const medium = useMediaQuery({ minWidth: 768, maxWidth: 992 });
+  const large = useMediaQuery({ minWidth: 992 });
 
   useEffect(() => {
     setTimeout(() => {
-      setLeftCurtainPlace(5);
-      setRightCurtainPlace(0);
-    }, 10);
-
-    setTimeout(() => {
       setLeftCurtainOpacity(1.0);
-      setImageFilter(100);
-    }, 300);
+    }, 500);
   }, []);
+
+  const storyCurtainStyles = {};
+  const headShotStyles = {};
+
+  if (extraSmall) {
+    headShotStyles.width = '50vw';
+    storyCurtainStyles.fontSize='16px'
+    storyCurtainStyles.top = '20vh';
+    storyCurtainStyles.lineHeight = '1.4rem';
+    storyCurtainStyles.color = 'white';
+    storyCurtainStyles.margin = '0';
+    headShotStyles.top = '0';
+    headShotStyles.height = '100vh';
+  }
+
+  if (medium) {
+    storyCurtainStyles.fontSize = '17px'
+    storyCurtainStyles.margin = '4rem 5rem 0 0';
+    storyCurtainStyles.lineHeight = '2.3rem';
+    storyCurtainStyles.color = 'darkBlue';
+    headShotStyles.width = '40vw';
+  }
+
+  if (large) {
+    headShotStyles.width = '20vw';
+    // console.log('medium')
+
+
+  }
 
   return (
     <Fragment>
       <div
         className={styles.headShotCurtain}
-        style={{
-          transform: `translateX(${leftCurtainPlace}vw)`,
-          opacity: leftCurtainOpacity,
-        }}
+        style={{ opacity: leftCurtainOpacity }}
       >
-        <img src={props.headshot} className={styles.headShot} />
+        <img src={props.headshot} className={styles.headShot} styles={headShotStyles}/>
       </div>
-      <div className={styles.storyCurtain} style={{ transform: `translateX(${rightCurtainPlace}vw)` }}>
+      <div className={styles.storyCurtain} style={storyCurtainStyles}>
         When I was a kid, I loved poetry. These days, I love a different kind of poetry- designing full stack apps and websites. I am happiest working in
         React and Node.js or Java, and I prefer building frontends that look original- not like so many of the cookie
         cutter sites we see every day. I've discovered that a world of digital poetry is possible
